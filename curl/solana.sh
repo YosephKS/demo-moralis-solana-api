545457d5cd61fc264ab81a6dc34640f723860894
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2022-04-17 21:33:09 +0700

    add solana.sh content

---
+++ curl/solana.sh
@@ -0,0 +1,4 @@
+curl -X 'GET' \
+  'https://solana-gateway.moralis.io/account/devnet/HsXZnF7Te7dZ5ijvGcDj3NWtxRBBaAuYQgh1oZLwAba2/portfolio' \
+  -H 'accept: application/json' \
+  -H 'X-API-Key: 3mTCXBymxPXJtGU0Hxt18v3XlLgqqeM7xr4TyxHGBSFBlKbl1y8d5v0ltdNcGVxW'
